12b1d2269a4570b461a2530de33c8110bd1aabf7
Rong Chen <rongc0723@gmail.com>
2024-09-02 06:40:18 -0400

    made more clean

--- bostonhacks-2024/app/Tracks.jsx
+++ bostonhacks-2024/app/Tracks.jsx
@@ -5,50 +5,44 @@ import planet3 from "../public/images/tracks/planet3.svg";
 import Image from "next/image";
 
 function Tracks() {
-  const boxStyle = {
-    "--background": "1 1 1",
-    "--highlight": "200 255 255",
+  
+  const planetsTextBoxStyle = {
+    "--background": "0,0,0,0.5",
+    "--highlight": "69 232 113",
     "--bg-color":
       "linear-gradient(rgb(var(--background)), rgb(var(--background)))",
     "--border-color": `linear-gradient(145deg,
           rgb(var(--highlight)) 0%,
           rgb(var(--highlight) / 0.3) 33.33%,
           rgb(var(--highlight) / 0.14) 66.67%,
-          rgb(var(--highlight) / 0.1) 100%)
+          rgb(var(--highlight) / 0.1) 5100%)
         `,
   };
 
+  const planetsTextBoxCSS = `rounded-lg border border-transparent text-center p-4 text-2xl
+      [background:padding-box_var(--bg-color),border-box_var(--border-color)]`;
+
   return (
     <div>
-      <h1 className="text-center text-4xl mb-5 pt-5">
+      <h1 className="text-center text-4xl mb-52 pt-5">
         __________ TRACKS __________
       </h1>
       <div className="flex flex-col">
-        <div className="w-1/3 flex flex-col items-center">
-          <h1
-            style={boxStyle}
-            className="rounded-lg border border-transparent text-center p-4 text-2xl
-      [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
-          >
+        <div className="w-1/3 flex flex-col items-center self-end">
+          <h1 style={planetsTextBoxStyle} className={planetsTextBoxCSS}>
             Reimagine Reality
           </h1>
           <Image src={planet1} alt="planet1" />
         </div>
         <div className="w-1/3 items-center flex flex-col">
-        <h1
-            style={boxStyle}
-            className="rounded-lg border border-transparent text-center p-4 text-2xl
-      [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
-          >            Cometcare
+          <h1 style={planetsTextBoxStyle} className={planetsTextBoxCSS}>
+            Cometcare
           </h1>
           <Image src={planet2} alt="planet2" />
         </div>
-        <div className="w-1/3 flex flex-col items-center">
-        <h1
-            style={boxStyle}
-            className="rounded-lg border border-transparent text-center p-4 text-2xl
-      [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
-          >            Interstellar Intelligence
+        <div className="w-1/3 flex flex-col items-center self-end mr-32">
+          <h1 style={planetsTextBoxStyle} className={planetsTextBoxCSS}>
+            Interstellar Intelligence
           </h1>
           <Image src={planet3} alt="planet3" />
         </div>
@@ -59,8 +53,8 @@ function Tracks() {
 
 export default Tracks;
 
-
-{/* <div
+{
+  /* <div
 style={{
   "--background": "1 1 1",
   "--highlight": "200 255 255",
@@ -77,4 +71,5 @@ className="flex aspect-[2/1] w-1/3 flex-col items-center justify-center rounded-
 [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
 >
 <p className="font-medium text-white">Testing gradient</p>
-</div> */}
+</div> */
+}
