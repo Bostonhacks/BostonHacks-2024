7877d5c1f50ab65bfbc95699aebc802e50bd9c68
Tiffany Liu <mazingmonkey2004@gmail.com>
2024-09-05 16:13:08 -0400

    added black rectangle background

--- bostonhacks-2024/components/LandingComponents/Sponsors.jsx
+++ bostonhacks-2024/components/LandingComponents/Sponsors.jsx
@@ -2,7 +2,7 @@ import { React } from 'react';
 import '../../public/fonts/fonts.css';
 import Image from 'next/image';
 
-import hold from '@/public/images/Q.svg';
+import hold from '@/public/images/saturn.png';
 import sponsorBackground from '@/public/images/sponsorBackground.svg';
 
 const Sponsors = () => {
@@ -20,7 +20,7 @@ const Sponsors = () => {
         {
             id: 3,
             name: 'Sponsor Three',
-            images: hold
+                images: hold
         },
         {
             id: 4,
@@ -41,31 +41,31 @@ const Sponsors = () => {
 
     return(
         // <div className="flex justify-center items-center flex-col mb-40">
-        <section id="sponsors" className="font-pp ssw-full flex flex-col items-center justify-center text-text-primary px-5 my-[10rem] mt-80 max-sm:mt-0 mb-40">
+        <section id="sponsors" className="font-pp ssw-full flex flex-col items-center justify-center text-text-primary px-5 my-[10rem] mt-80 max-sm:mt-0 mb-40 text-white mt-10 w-full max-w-4xl bg-black bg-opacity-50 p-8 rounded-lg shadow-lg  mx-auto ">
                 <div className="flex items-center w-full max-w-xl relative">
                     <div className="flex-1 border-b-2 border-white mt-[2rem]"></div>
                     <h2 className="text-center text-3xl md:text-5xl pb-1 px-12">OUR SPONSORS</h2>
                     <div className="flex-1 border-b-2 border-white mt-[2rem]"></div>
                 </div>
             <div className="grid z-10 grid-cols-2 lg:grid-cols-2 gap-14 lg:gap-20 mt-20 mx-[5vw]">
-            {sponsors.map((sponsor) => (
-            <div key={sponsor.id} className="relative flex items-center justify-center">
-                <Image
-                src={sponsorBackground}
-                alt="sponsor"
-                className="w-32 h-32 mx-auto"
-                />
-                <Image
-                src={sponsor.images}
-                alt={`Sponsor ${sponsor.id}`}
-                className="absolute w-auto h-auto top-0 left-0 object-cover scale-75"
-                />
-                    <span className={`absolute right-[-20px] top-50% transform -translate-y-1/2  text-xs text-center ${sponsor.id % 2 === 1 ? 'left-[-20px]' : 'right-[-20px]'}`}  style={{writingMode: 'vertical-rl', transform: 'rotate(180deg)', whiteSpace: 'nowrap'}}>
-                        {sponsor.name}
-                    </span>
+                {sponsors.map((sponsor) => (
+                <div key={sponsor.id} className="relative flex items-center justify-center">
+                    <Image
+                    src={sponsorBackground}
+                    alt="sponsor"
+                    className="w-32 h-32 mx-auto"
+                    />
+                    <Image
+                    src={sponsor.images}
+                    alt={`Sponsor ${sponsor.id}`}
+                    className="absolute w-auto h-auto top-0 left-0 object-cover scale-75"
+                    />
+                        <span className={`absolute right-[-20px] top-50% transform -translate-y-1/2  text-xs text-center ${sponsor.id % 2 === 1 ? 'left-[-20px]' : 'right-[-20px]'}`}  style={{writingMode: 'vertical-rl', transform: 'rotate(180deg)', whiteSpace: 'nowrap'}}>
+                            {sponsor.name}
+                        </span>
+                </div>
+                ))}
             </div>
-            ))}
-        </div>
         </section>
     //   </div>
     )
