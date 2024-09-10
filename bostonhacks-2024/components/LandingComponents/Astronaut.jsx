034ccaefce66741eaaad81ac8c9febc66fb90260
Tiffany Liu <mazingmonkey2004@gmail.com>
2024-09-09 20:57:17 -0400

    adjusted apply button position and cleaned up code

--- bostonhacks-2024/components/LandingComponents/Astronaut.jsx
+++ bostonhacks-2024/components/LandingComponents/Astronaut.jsx
@@ -1,11 +1,6 @@
 import { React } from 'react';
 import Image from 'next/image';
 
-import SpaceGuy from '@/public/images/astronaut.png';
-import Rocket from '@/public/images/rocket.svg';
-import v1 from '@/public/images/vector1.svg';
-import v2 from '@/public/images/vector2.svg';
-import ellipses from '@/public/images/ellipses.svg';
 import section from '@/public/images/section.svg';
 import ApplyButton from "@/public/images/Apply2.svg";
 
@@ -20,7 +15,7 @@ const Astronaut = () => {
                 objectFit="contain"
                 style={{ zIndex: 1 }}
             />
-            <div className="cursor-pointer absolute left-[50%] top-[50%] transform -translate-x-1/2 -translate-y-1/2" style={{ zIndex: 2 }}>
+            <div className="cursor-pointer absolute left-[50%] top-[45%] transform -translate-x-1/2 -translate-y-1/2" style={{ zIndex: 2 }}>
                 <Image
                     src={ApplyButton}
                     alt="Apply Button"
@@ -28,31 +23,6 @@ const Astronaut = () => {
                     height={200}  // Adjust based on your SVG size
                 />
             </div>
-            {/* <Image 
-                src={SpaceGuy}
-                alt="astronaut"
-                className="absolute w-[600px] left-[5%] transform rotate-[-5deg]"
-            />
-            <Image  
-                src={Rocket}
-                alt="rocket"
-                className="absolute w-[100px] bottom-[20%] right-[20%]"
-            />
-            <Image
-                src={v1}
-                alt="rocket path"
-                className="absolute w-[500px] top-[27%] right-[18%]"
-            />
-            <Image 
-                src={v2}
-                alt="rocket path"
-                className="absolute w-[500px] top-[30%] right-[20%]"
-            />
-            <Image 
-                src={ellipses}
-                alt="ellipses"
-                className="absolute w-[150px] top-[85%] left-[50%]"
-            /> */}
         </div> 
     );
 };
